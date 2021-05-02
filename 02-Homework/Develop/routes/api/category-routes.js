f2ad80e4db9bea5740729acbe2e5d94269ebd781
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-05-01 21:38:30 -0500

    established category routes

--- 02-Homework/Develop/routes/api/category-routes.js
+++ 02-Homework/Develop/routes/api/category-routes.js
@@ -13,23 +13,49 @@ router.get('/', (req, res) => {
     res.json(categories)
   })
   // be sure to include its associated Products
+
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  const id = req.params.id 
+  Category.findByPk(id)
+
+  .then(category => {
+    res.json(category)
+  })
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  const category_name = req.body.category_name
+  Category.create({category_name})
+  .then(category =>{
+    res.json(category)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  const category_name = req.body.category_name
+  const id = req.params.id
+  Category.update({category_name},{where:{id}})
+  .then(category => {
+    res.json(category)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  const id = req.params.id
+  Category.destroy({
+    where:{id}
+  })
+    .then(category=>{
+      res.json(category)
+    
+  })
 });
 
 module.exports = router;
